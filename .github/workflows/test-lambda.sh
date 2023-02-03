dce6deb3b8e6d7fd62e1cb0a8ba986de0be326d4
Sasa Milic <milichkam@gmail.com>
2023-02-03 16:24:41 -0500

    Update test-lambda.sh

--- .github/workflows/test-lambda.sh
+++ .github/workflows/test-lambda.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #cd ../..
 sam local start-lambda &
-  aws lambda invoke \
-    --function-name "NatalChartGenFunction" \
-    --payload '{"queryStringParameters":{"datetime_string":"04/01/91 17:55:00","location_string":"zenica"}}' \
-    out.json \
-    --endpoint-url "http://app:8080" \
-    --no-verify-ssl \
-    --cli-binary-format raw-in-base64-out
+aws lambda invoke \
+  --function-name "NatalChartGenFunction" \
+  --payload '{"queryStringParameters":{"datetime_string":"04/01/91 17:55:00","location_string":"zenica"}}' \
+  out.json \
+  --endpoint-url "http://127.0.0.1:3001/" \
+  --no-verify-ssl \
+  --cli-binary-format raw-in-base64-out
 OUTPUT=$(jq '.body' out.json | jq -r | jq '.preSignedUrl')
 [[ "$OUTPUT" =~ ^\"https.*\"$ ]] && exit 0 || exit 1
 rm -f out.json
-cd -
+#cd -
