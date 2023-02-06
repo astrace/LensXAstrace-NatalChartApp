090a80fb169be5dc10557148819d06337b1109c7
Sasa Milic <milichkam@gmail.com>
2023-02-06 15:56:46 -0500

    parameterizing image display

---
+++ natal_chart_gen/image_params.py
@@ -0,0 +1,12 @@
+# NOTE: In most instances, params are a function of
+# background image height/width in order to
+# minimize "degrees of freedom"
+
+# distance of planets from center as % of background image
+PLANET_RADIUS = 0.28
+# planet size as % of background image
+PLANET_SIZE = 0.055
+# radius of inner circle containing house numbers
+HOUSE_NUMBER_RADIUS = 0.45
+# radius of central astrace logo
+LOGO_RADIUS = 0.07
