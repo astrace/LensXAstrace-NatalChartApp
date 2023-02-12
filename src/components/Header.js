cfc762c07854ad24f3dc5940c8c1c0f68408ae3c
Sasa Milic <milichkam@gmail.com>
2023-02-12 15:13:53 -0500

    made header responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Image from 'next/image';
 import styles from '@/styles/Home.module.css'
+import {useWindowWidth} from '@react-hook/window-size'
 
-function Header() {
+const Header = () => {
+  /* We use a different logo in the header when viewport is small */
+  const [isMobile, setIsMobile] = useState(false);
+
+  // see: https://blog.sethcorker.com/question/how-to-solve-referenceerror-next-js-window-is-not-defined/
+  useEffect(() => {
+    setIsMobile(window.innerWidth <= 428 ? true : false);
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 428 ? true : false);
+    };
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <header className={styles.header}>
       <div className={styles["header-logo"]}>
-        <Image
-          src="/astrace_text.svg"
-          width={200}
-          height={30}
-        />
+        {isMobile? (
+          <Image
+            src="/logo.svg"
+            width={200}
+            height={30}
+          />
+        ) : (
+          <Image
+            src="/astrace_text.svg"
+            width={200}
+            height={30}
+          />
+        )}
       </div>
     </header>
   )
