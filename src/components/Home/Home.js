2c124cb760f305f5246a5d0a088c8e535f72e7fe
Sasa Milic <milichkam@gmail.com>
2023-02-19 17:25:12 -0500

    refactoring - home and form as components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,140 +1,65 @@
-import React, { useState, Component } from "react";
-//import {ethers} from 'ethers';
-import { useRouter } from 'next/router';
+import React, { useState } from 'react';
 import { useWeb3React } from '@web3-react/core';
-import Head from 'next/head';
-import Image from 'next/image';
-import Header from '../components/Header/Header.js';
-import Button from '../components/Buttons/Button.js';
-import Modal from '../components/Modal/Modal.js';
-import Footer from '../components/Footer/Footer.js';
-import styles from '@/styles/Home.module.css';
+import { InjectedConnector } from '@web3-react/injected-connector';
+import { switch2Polygon} from '../../utils/networkConnect.js';
+import Button from '../Buttons/Button.js';
+import Modal from '../Modal/Modal.js';
+import styles from './Home.module.css'
 // logos
-import background from '../../public/background-image.png';
-import ethereum_icon from '../icons/ethereum.svg';
-import wallet_connect_icon from '../icons/wallet_connect.svg';
+import ethereum_icon from '../../icons/ethereum.svg';
+import wallet_connect_icon from '../../icons/wallet_connect.svg';
 
-// see: https://stackoverflow.com/questions/57027469/how-to-use-userouter-from-next-js-in-a-class-component
-function HomeWithRouter(props) {
-  const router = useRouter()
-  return <Home {...props} router={router} />
-}
+const POLYGON_CHAIN_ID = 137;
 
-class Home extends React.Component {
+export default function Home() {
+  const [showModal, setShowModal] = useState(false);
+  const { activate, deactivate, active, account, library } = useWeb3React();
+  
+  const injected = new InjectedConnector({
+    supportedChainIds: [1, 3, 4, 5, 42, 137],
+  })
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      router: props.router,
-      showModal: false,
-      defaultAccount: null,
-      userBalance: null,
-    };
-  };
-
-	connectWalletHandler = () => {
-		if (window.ethereum && window.ethereum.isMetaMask) {
-			console.log('MetaMask Here!');
-
-			window.ethereum.request({ method: 'eth_requestAccounts'})
-			.then(result => {
-				this.accountChangedHandler(result[0]);
-				this.getAccountBalance(result[0]);
-        this.state.router.push({
-          pathname: '/form',
-          query: {account: result[0]}  
-        }, '/form');
-			})
-			.catch(error => {
-			  console.log("ERROR MSG");
-			  console.log(error.message);
-			});
-      //window.location.href = "/form";
-		} else {
-			console.log('Need to install MetaMask');
-		}
-	}
-
-  // update account, will cause component re-render
-	accountChangedHandler = (newAccount) => {
-	  this.setState({defaultAccount: newAccount});
-		this.getAccountBalance(newAccount.toString());
-	}
-
-	getAccountBalance = (account) => {
-		window.ethereum.request({method: 'eth_getBalance', params: [account, 'latest']})
-		.then(balance => {
-		  console.log(balance);
-	    this.setState({userBalance: balance});
-		})
-		.catch(error => {
-		  console.log("ERRROR");
-		  console.log(error.message);
-		});
-	};
-
-	chainChangedHandler = () => {
-		// reload the page to avoid any errors with chain change mid use of application
-		window.location.reload();
-	};
-
-
-  componentDidMount() {
-	  // listen for account changes
-	  window.ethereum.on('accountsChanged', this.accountChangedHandler);
-	  window.ethereum.on('chainChanged', this.chainChangedHandler);
+  async function connectBrowserWallet() {
+    try {
+      await activate(injected);
+      localStorage.setItem('isBrowserWalletConnected', true);
+      setShowModal(false);
+    } catch (ex) {
+      console.log(ex);
+    }
   }
 
-
-  render() { return (
-    <>
-      <Head>
-        <title>Launch LENS astrological profile</title>
-        <meta name="description" content="Launch your Lens astrological profile" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className={styles.bgWrap}>
-        <Image
-          alt="background"
-          src={background}
-          placeholder="blur"
-          quality={100}
-          fill
-          sizes="100vw"
-          style={{
-            objectFit: 'cover',
-          }}
-        />
+  return (
+    <div className={styles["main-text"]}>
+      <h1>Launch your Lens astrological profile</h1>
+      <p style={{paddingBottom: 10}}>
+        Connect your wallet that holds the Lens profile to mint your 
+        Soulbound natal chart NFT and retrieve your astro profile.
+      </p>
+      {!active && <Button text="Connect Wallet" onClick={() => setShowModal(true)}/>}
+      <Modal
+        title="Connect Wallet"
+        buttons={[
+          <Button onClick={connectBrowserWallet} text="Browser Wallet" src={ethereum_icon}  />,
+          <Button text="WalletConnect" src={wallet_connect_icon} />
+        ]}
+        onClose={() => setShowModal(false) }
+        show={showModal}
+      />
+      {(active && window.ethereum.networkVersion == POLYGON_CHAIN_ID) && <Button text="Continue" />}
+      {
+        (active && window.ethereum.networkVersion != POLYGON_CHAIN_ID)
+        && <Button onClick={() => switch2Polygon(library)} text="Switch Network" />
+      }
+      <p style={{paddingTop: 18}}>
+        Don’t have a Lens profile?
+        &nbsp;
+        <a href="https://www.lens.xyz/" style={{color: 'red', fontWeight: 400}}>See how to get it.</a>
+      </p>
+      <div style={{color: "white"}}>
+        <span>Active: {active.toString()}</span>
+        <span>Account: {account}</span>
       </div>
-      <main className={styles.main}>
-        <Header showWallet={false} />
-        <div className={styles["main-text"]}>
-          <h1>Launch your Lens astrological profile</h1>
-          <p style={{paddingBottom: 10}}>
-            Connect your wallet that holds the Lens profile to mint your 
-            Soulbound natal chart NFT and retrieve your astro profile.
-          </p>
-          <Button text="Connect Wallet" onClick={() => this.setState({ showModal: true }) }/>
-          <Modal
-            title="Connect Wallet"
-            buttons={[
-              <Button onClick={this.connectWalletHandler} text="Browser Wallet" src={ethereum_icon}  />,
-              <Button text="WalletConnect" src={wallet_connect_icon} />
-            ]}
-            onClose={() => this.setState({ showModal: false }) }
-            show={this.state.showModal}
-          />
-          <p style={{paddingTop: 18}}>
-            Don’t have a Lens profile?
-            &nbsp;
-            <a href="https://www.lens.xyz/" style={{color: 'red', fontWeight: 400}}>See how to get it.</a>
-          </p>
-        </div>
-          <Footer/>
-      </main>
-    </>
-  )};
+    </div>
+  )
 }
-
-export default HomeWithRouter;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,14 @@ import { useWeb3React } from '@web3-react/core';
 import { InjectedConnector } from '@web3-react/injected-connector';
 import { switch2Polygon} from '../utils/networkConnect.js';
 import Head from 'next/head';
-import Image from 'next/image';
+import Background from '../components/Background/Background.js';
 import Header from '../components/Header/Header.js';
+import Home from '../components/Home/Home.js';
 import Button from '../components/Buttons/Button.js';
 import Modal from '../components/Modal/Modal.js';
 import Footer from '../components/Footer/Footer.js';
 import styles from '@/styles/Home.module.css';
 // logos
-import background from '../../public/background-image.png';
 import ethereum_icon from '../icons/ethereum.svg';
 import wallet_connect_icon from '../icons/wallet_connect.svg';
 
@@ -18,102 +18,23 @@ const POLYGON_CHAIN_ID = 137;
 
 export default function Index(props) {
   const [showModal, setShowModal] = useState(false);
-
-  const { active, account, library, connector, activate, deactivate } = useWeb3React();
-
-  const injected = new InjectedConnector({
-    supportedChainIds: [1, 3, 4, 5, 42, 137],
-  })
-
-  async function connectBrowserWallet() {
-    try {
-      await activate(injected);
-      localStorage.setItem('isBrowserWalletConnected', true);
-      setShowModal(false);
-    } catch (ex) {
-      console.log(ex);
-    }
-  }
-
-  async function disconnect() {
-    try {
-      deactivate()
-      localStorage.setItem('isBrowserWalletConnected', false)
-    } catch (ex) {
-      console.log(ex)
-    }
-  }
-
-  // detect connection on refresh
-  useEffect(() => {
-    const connectWalletOnPageLoad = async () => {
-      if (localStorage?.getItem('isBrowserWalletConnected') === 'true') {
-        try {
-          await activate(injected)
-        } catch (ex) {
-          console.log(ex)
-        }
-      }
-    }
-    connectWalletOnPageLoad()
-  }, [])
+  
+  // TODO: change to enum
+  const [whichPage, setWhichPage] = useState("home");
 
   return (
     <>
       <Head>
-        <title>Launch LENS astrological profile</title>
+        <title>Astrace X Lens</title>
         <meta name="description" content="Launch your Lens astrological profile" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.bgWrap}>
-        <Image
-          alt="background"
-          src={background}
-          placeholder="blur"
-          quality={100}
-          fill
-          sizes="100vw"
-          style={{
-            objectFit: 'cover',
-          }}
-        />
-      </div>
+      <Background />
       <main className={styles.main}>
         <Header showWallet={false} />
-        <div className={styles["main-text"]}>
-          <h1>Launch your Lens astrological profile</h1>
-          <p style={{paddingBottom: 10}}>
-            Connect your wallet that holds the Lens profile to mint your 
-            Soulbound natal chart NFT and retrieve your astro profile.
-          </p>
-          {!active && <Button text="Connect Wallet" onClick={() => setShowModal(true)}/>}
-          <Modal
-            title="Connect Wallet"
-            buttons={[
-              <Button onClick={connectBrowserWallet} text="Browser Wallet" src={ethereum_icon}  />,
-              <Button text="WalletConnect" src={wallet_connect_icon} />
-            ]}
-            onClose={() => setShowModal(false) }
-            show={showModal}
-          />
-          {(active && window.ethereum.networkVersion == POLYGON_CHAIN_ID) && <Button text="Continue" />}
-          {
-            (active && window.ethereum.networkVersion != POLYGON_CHAIN_ID)
-            && <Button onClick={() => switch2Polygon(library)} text="Switch Network" />
-          }
-          <p style={{paddingTop: 18}}>
-            Don’t have a Lens profile?
-            &nbsp;
-            <a href="https://www.lens.xyz/" style={{color: 'red', fontWeight: 400}}>See how to get it.</a>
-          </p>
-          <div style={{color: "white"}}>
-            <span>Active: {active.toString()}</span>
-            <span>Account: {account}</span>
-            <span>Active: {active}</span>
-          </div>
-        </div>
-          <Footer/>
+        {whichPage == "home" && <Home />}
+        <Footer/>
       </main>
     </>
   )
