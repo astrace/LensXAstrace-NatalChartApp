971e1fc84c7b72d52b255f1a43a4e52c042962a0
Sasa Milic <milichkam@gmail.com>
2023-02-23 22:43:26 -0500

    removing unnecessary imports from index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,13 @@
-import React, { useState, useEffect, useCallback } from "react";
-import { useWeb3React } from '@web3-react/core';
-import { InjectedConnector } from '@web3-react/injected-connector';
-import { switch2Polygon} from '../utils/networkConnect.js';
+import React, { useState,  useCallback } from "react";
 import Head from 'next/head';
 import Background from '../components/Background/Background.js';
 import Header from '../components/Header/Header.js';
 import Home from '../components/Home/Home.js';
 import Form from '../components/Form/Form.js';
-import Button from '../components/Buttons/Button.js';
-import Modal from '../components/Modal/Modal.js';
 import Footer from '../components/Footer/Footer.js';
 import styles from '@/styles/Index.module.css';
-// logos
-import ethereum_icon from '../icons/ethereum.svg';
-import wallet_connect_icon from '../icons/wallet_connect.svg';
-
-
 
 export default function Index(props) {
-  const [showModal, setShowModal] = useState(false);
   // TODO: change to enum
   const [page, setPage] = useState("home");
 
