27153059064e395e2e043da5f920031874362e8c
User <sean.al.baroudi@gmail.com>
2023-03-08 21:12:33 +0000

    ChatGPT made a few docstrings. Now I must check them

--- natal_chart_gen/prototype.py
+++ natal_chart_gen/prototype.py
@@ -17,6 +17,27 @@ import utils
 swe.set_ephe_path('./assets/ephe')
 
 class Planet:
+    """
+    A class representing a planet in the solar system.
+
+    Attributes:
+    - name: A string representing the name of the planet.
+    - sign: A string representing the zodiac sign where the planet is located.
+    - images: A dictionary containing the paths to the planet image and the zodiac sign image.
+    - position: A tuple (x, y) representing the current position of the planet in the sky.
+    - abs_pos: A float representing the absolute position of the planet in the sky, measured in degrees counterclockwise from 0 degree Aries on the ecliptic.
+    - dpos: A float representing the change in position of the planet in the sky, measured in degrees.
+
+    Methods:
+    - __str__(): Returns a string representation of the planet object, including the planet name, zodiac sign, and absolute position.
+
+    Note:
+    - The `images` attribute is a dictionary containing two keys: 'planet' and 'sign', each corresponding to the path to the planet image and the zodiac sign image, respectively.
+    - The `position` attribute is a tuple representing the current x and y coordinates of the planet's position in the sky.
+    - The `abs_pos` attribute is a float representing the absolute position of the planet in the sky, measured in degrees counterclockwise from 0 degree Aries on the ecliptic.
+    - The `dpos` attribute is a float representing the change in position of the planet in the sky, measured in degrees.
+    """
+
     def __init__(self, name, position, abs_pos, sign):
         self.name = name
         self.sign = sign
@@ -34,13 +55,40 @@ class Planet:
         )
 
 class Natal_Chart:
+    """
+    A class representing a natal chart object.
+    
+    Attributes:
+    -----------
+    required_objects : frozenset
+        A set of all required planet names.
+    objects : dict
+        A dictionary containing planet names as keys and corresponding Planet objects as values.
+    jd : float or None
+        Julian day number representing the date and time of the natal chart. If None, it is assumed that the 
+        chart is for the current date and time.
+    """
     required_objects = frozenset([
         "Sun", "Moon", "Venus", "Mars",
         "Jupiter", "Saturn", "Uranus", "Neptune",
         "Pluto", "North Node", "Chiron",
         "Asc", "Mc"
     ])
+
     def __init__(self, planets, jd=None):
+        """
+        Parameters:
+        -----------
+        planets : list
+            A list of Planet objects representing the planets in the natal chart.
+        jd : float or None, optional (default=None)
+            Julian day number representing the date and time of the natal chart. If None, it is assumed that the 
+            chart is for the current date and time.
+        
+        Raises:
+        -------
+        Exception : if any planet/object is missing in the chart
+        """
         self.objects = {}
         for p in planets:
             self.objects[p.name] = p
@@ -169,7 +217,16 @@ def set_background(asc, load_image_fn=utils.load_image):
     return bg_im
 
 def get_center(bg_size, fg_size):
-    # TODO: description; needed in order to center image
+    """
+    Returns the offset necessary to center an image with size `fg_size` on a background image with size `bg_size`.
+
+    Args:
+    - bg_size: A tuple (width, height) representing the size of the background image.
+    - fg_size: A tuple (width, height) representing the size of the image to be centered.
+
+    Returns:
+    A tuple (offset_w, offset_h) representing the offset necessary to center the image on the background image.
+    """
     bg_w, bg_h = bg_size
     img_w, img_h = fg_size
     offset_w = (bg_w - img_w) // 2
@@ -177,7 +234,17 @@ def get_center(bg_size, fg_size):
     return offset_w, offset_h
 
 def resize_image(im, bg_size, p):
-    # resize planet as `p` percent of blackground image
+    """
+    Resizes the input image `im` to a new size that is `p` percent of the `bg_size` image size.
+
+    Args:
+    - im: The input image to be resized.
+    - bg_size: A tuple (width, height) representing the size of the background image.
+    - p: A float representing the percentage of the `bg_size` image size to which the `im` should be resized.
+
+    Returns:
+    The resized image as a PIL Image object.
+    """
     new_width = p * bg_size[0]
     new_height = (new_width / im.size[0]) * im.size[1]
     new_size = tuple(map(int, (new_width, new_height)))
@@ -185,6 +252,25 @@ def resize_image(im, bg_size, p):
 
 def get_coordinates(asc, a, b, r, theta):
     # get (x,y) location of 0 degree Aries
+
+    """
+    Calculates the position of a point that is `r` units away from a center point (`a`, `b`) at an angle of `theta` degrees from the point that is located `deg` degrees counterclockwise from 0 degree Aries on the ecliptic.
+
+    Args:
+    - asc: A string representing the zodiac sign where 0 degree Aries is located.
+    - a: A float representing the x-coordinate of the center point.
+    - b: A float representing the y-coordinate of the center point.
+    - r: A float representing the distance from the center point to the point of interest.
+    - theta: A float representing the angle between the line connecting the center point and the point of interest and the x-axis.
+
+    Returns:
+    A tuple (u, v) representing the position of the point of interest after being rotated around the center point (`a`, `b`) by an angle `theta` degrees counterclockwise.
+
+    Note:
+    - The `asc` argument should be a string representing a zodiac sign, where 0 degree Aries is located. It is used to calculate the starting position of the point on the ecliptic.
+    - The function `rotate()` is used to calculate the resulting position after rotation around the center point.
+    """
+
     deg = -90 - constants.SIGNS.index(asc) * 30
     x = a + r * math.sin(math.radians(deg))
     y = b + r * math.cos(math.radians(deg))
@@ -196,6 +282,21 @@ def rotate(a, b, s, t, deg):
     # -> circle with center (a,b)
     # return resulting position when rotating `deg`
     # degrees from starting position (s,t)
+
+    """
+    Rotates the point (`s`, `t`) around the point (`a`, `b`) by `deg` degrees and returns the resulting position.
+
+    Args:
+    - a: A float representing the x-coordinate of the center of the circle.
+    - b: A float representing the y-coordinate of the center of the circle.
+    - s: A float representing the x-coordinate of the point to be rotated.
+    - t: A float representing the y-coordinate of the point to be rotated.
+    - deg: A float representing the angle by which the point (`s`, `t`) should be rotated in degrees.
+
+    Returns:
+    A tuple (u, v) representing the resulting position of the rotated point.
+    """
+
     cos_theta = math.cos(math.radians(deg))
     sin_theta = math.sin(math.radians(deg))
     u = a + (s - a) * cos_theta + (t - b) * sin_theta
