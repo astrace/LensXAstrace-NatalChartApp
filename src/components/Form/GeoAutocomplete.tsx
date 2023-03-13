387cc0de340c2f155c8901579c72233510fe345c
Sasa Milic <milichkam@gmail.com>
2023-03-13 20:38:39 +0100

    bug fix - missing type

--- src/components/Form/GeoAutocomplete.tsx
+++ src/components/Form/GeoAutocomplete.tsx
@@ -27,6 +27,19 @@ function loadScript(src: string, position: HTMLElement | null, id: string) {
 
 const autocompleteService = { current: null };
 
+interface MainTextMatchedSubstrings {
+  offset: number;
+  length: number;
+}
+interface StructuredFormatting {
+  main_text: string;
+  secondary_text: string;
+  main_text_matched_substrings?: readonly MainTextMatchedSubstrings[];
+}
+interface PlaceType {
+  description: string;
+  structured_formatting: StructuredFormatting;
+}
 
 export default function GoogleMaps(props) {
   const [inputValue, setInputValue] = React.useState('');
